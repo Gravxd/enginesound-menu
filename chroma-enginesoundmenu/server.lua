aba54fe36c3e325f643fc567516b72f96ac3e6e1
Grav <75702884+Gravxd@users.noreply.github.com>
2024-03-14 19:02:36 +0000

    tweak(server) custom permission function

--- chroma-enginesoundmenu/server.lua
+++ chroma-enginesoundmenu/server.lua
@@ -1,12 +1,19 @@
+local function HasPermission(src)
+    -- your permission function here - you can integrate your framework for jobs/perms etc
+    return IsPlayerAceAllowed(src, 'enginesoundmenu')
+end
+
 RegisterCommand("enginesound", function(source, args, rawCommand)
-    TriggerClientEvent("Chroma:EngineSounds:OpenMenu", source)
-end, true)
+    if HasPermission(source) then
+        TriggerClientEvent("Chroma:EngineSounds:OpenMenu", source)
+    end
+end, false)
 
 RegisterServerEvent("Chroma:EngineSounds:ChangeEngineSound", function(data)
 
     local entity = NetworkGetEntityFromNetworkId(data.net)
     if not DoesEntityExist(entity) then return end
 
-    Entity(entity).state['enginesound'] = data.sound
+    Entity(entity).state['vehdata:sound'] = data.sound
 
 end)
