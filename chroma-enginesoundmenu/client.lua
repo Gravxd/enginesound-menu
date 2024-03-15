2cd65a091fdc34910578443fe1e5a8e9cbd6da60
Grav <75702884+Gravxd@users.noreply.github.com>
2024-03-15 12:43:55 +0000

    tweak(client) notification support + chat suggestion

--- chroma-enginesoundmenu/client.lua
+++ chroma-enginesoundmenu/client.lua
@@ -3,8 +3,17 @@ for k, v in pairs(Config.EngineSounds) do
     DisplayLabels[#DisplayLabels + 1] = k
 end
 
-local Index = 1
+local function Notify(msg, type)
+    -- you can edit this to whatever you want, by default it uses ox_lib notifications
+    lib.notify({
+        title = 'chroma-enginesoundmenu',
+        description = msg,
+        type = type,
+        position = 'center-right',
+    })
+end
 
+local Index = 1
 lib.registerMenu({
     id = 'engine_sound_menu',
     title = 'Engine Sound Menu',
@@ -16,17 +25,27 @@ lib.registerMenu({
         { label = 'Change Engine Sound', icon = 'arrows-up-down-left-right', values = DisplayLabels },
     }
 }, function(selected, scrollIndex, args)
-    if not cache.vehicle or cache.seat ~= -1 then return end
+    if not cache.vehicle or cache.seat ~= -1 then
+        return Notify('You need to be driving a vehicle to use this!', 'error') 
+    end
+    
     TriggerServerEvent('Chroma:EngineSounds:ChangeEngineSound', {
         net = VehToNet(cache.vehicle),
         sound = Config.EngineSounds[DisplayLabels[scrollIndex]]
     })
+
+    Notify(string.format('Engine sound changed to: %s', DisplayLabels[scrollIndex]), 'success')
+
 end)
 
 RegisterNetEvent("Chroma:EngineSounds:OpenMenu", function()
-    if not cache.vehicle or cache.seat ~= -1 then return end
+    if not cache.vehicle or cache.seat ~= -1 then
+        return Notify('You need to be driving a vehicle to use this!', 'error') 
+    end
+
     lib.setMenuOptions('engine_sound_menu', { label = 'Change Engine Sound', icon = 'arrows-up-down-left-right', values = DisplayLabels, defaultIndex = Index }, 1)
     lib.showMenu('engine_sound_menu')
+
 end)
 
 AddStateBagChangeHandler("vehdata:sound", nil, function(bagName, key, value)
@@ -44,3 +63,5 @@ local keybind = lib.addKeybind({
         ExecuteCommand('enginesound')
     end,
 })
+
+TriggerEvent('chat:addSuggestion', '/enginesound', 'Open the Engine Sound Menu!')
