354609c0895416351ff6083289970956760dedd9
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-03-15 06:29:00 -0500

    Readability on Config
    Updated the Config for more readability

--- chroma-enginesoundmenu/config.lua
+++ chroma-enginesoundmenu/config.lua
@@ -1,8 +1,9 @@
-Config = {}
-
-Config.Keybind = "" -- E.G F7 ---> https://docs.fivem.net/docs/game-references/controls/
-
-Config.EngineSounds = {
-    -- Engine Sound Name/Label --> Hash of engine audio (what you'd normally put in vehicles.meta)
-    ['BMW S63 4.4L V8'] = 's63b44',
+Config = {
+    keybind = "", -- E.G F7 ---> https://docs.fivem.net/docs/game-references/controls/
+    EngineSounds = {
+        -- Engine Sound Name/Label --> Hash of engine audio (what you'd normally put in vehicles.meta)
+        ['BMW S63 4.4L V8'] = 's63b44',
+    }
 }
+
+
