0958cb0d8bfc6f6c42b9e21fb5f19a6e5e97f389
Grav <75702884+Gravxd@users.noreply.github.com>
2024-03-15 12:44:07 +0000

    tweak(server) notification support

--- chroma-enginesoundmenu/server.lua
+++ chroma-enginesoundmenu/server.lua
@@ -3,15 +3,21 @@ local function HasPermission(src)
     return IsPlayerAceAllowed(src, 'enginesoundmenu')
 end
 
+local function Notify(src, msg, type)
+    -- you can edit this to whatever you want, by default it uses ox_lib notifications
+    TriggerClientEvent("ox_lib:notify", src, {
+        description = msg,
+        title = 'chroma-enginesoundmenu',
+        type = type,
+        position = 'center-right',
+    })
+end
+
 RegisterCommand("enginesound", function(source, args, rawCommand)
     if HasPermission(source) then
         TriggerClientEvent("Chroma:EngineSounds:OpenMenu", source)
     else
-        TriggerClientEvent('chat:addMessage', source, {
-            color = { 255, 0, 0 },
-            multiline = true,
-            args = { "[ERROR]", "You don't have permission to use this command." } -- updated to let the player know they dont have permission
-        })
+        Notify(source, 'You do not have permission to use this command!', 'error')
     end
 end, false)
 
