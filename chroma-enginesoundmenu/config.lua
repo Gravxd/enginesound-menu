8a783a6338f299774e9cc82b478af3ab5547e69b
Freech <144473764+freech-dev@users.noreply.github.com>
2024-03-16 14:02:34 +0000

    Update config.lua

--- chroma-enginesoundmenu/config.lua
+++ chroma-enginesoundmenu/config.lua
@@ -1,5 +1,10 @@
 Config = {
     Keybind = "", -- E.G F7 ---> https://docs.fivem.net/docs/game-references/controls/
+    -- 1 = Chat
+    -- 2 = oxNotify
+    -- 3 = okokNotify
+    -- 4 = Custom notification (Check client_config.lua)
+    Notification = "1",
     EngineSounds = {
         -- Engine Sound Name/Label --> Hash of engine audio (what you'd normally put in vehicles.meta)
         ['BMW S63 4.4L V8'] = 's63b44',
