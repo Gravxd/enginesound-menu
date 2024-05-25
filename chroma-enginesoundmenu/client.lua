1c03a9cd3557b373ee66955bac25b59bafe17d23
David Tanasescu <davidtanasescu09@gmail.com>
2024-05-25 13:02:36 +0200

    feat: Favourite Engine Sounds + Search
    - Fixed menu closing even if it was set in config to not close
    - Using KVP's (only in client.lua as request by @Gravxd) instead of .JSON
    - Reverted server.lua back to main branch

--- chroma-enginesoundmenu/client.lua
+++ chroma-enginesoundmenu/client.lua
@@ -1,5 +1,5 @@
 local DisplayLabels = {}
-for k, v in pairs(Config.EngineSounds) do
+for k, _ in pairs(Config.EngineSounds) do
     DisplayLabels[#DisplayLabels + 1] = k
 end
 
@@ -7,24 +7,104 @@ local Index = 1
 local Favourites = {}
 
 local function loadFavourites()
-    TriggerServerEvent('Chroma:EngineSounds:LoadFavourites')
+    local jsonFavourites = GetResourceKvpString('favouriteEngineSounds')
+    if jsonFavourites then
+        Favourites = json.decode(jsonFavourites)
+    end
 end
 
 local function saveFavourites()
-    TriggerServerEvent('Chroma:EngineSounds:SaveFavourites', Favourites)
+    SetResourceKvp('favouriteEngineSounds', json.encode(Favourites))
 end
 
 loadFavourites()
 
-RegisterNetEvent('Chroma:EngineSounds:ReceiveFavourites', function(favourites)
-    Favourites = favourites or {}
-end)
+local function showEngineSoundMenu()
+    lib.setMenuOptions('engine_sound_menu',
+        {
+            label = 'Change Engine Sound',
+            icon = 'arrows-up-down-left-right',
+            values = DisplayLabels,
+            defaultIndex = Index,
+            close = Config.CloseOnSelect
+        },
+        1)
+    lib.showMenu('engine_sound_menu')
+end
+
+local function showRemoveFavouritesMenu()
+    local removeOptions = {}
+    for fav, _ in pairs(Favourites) do
+        table.insert(removeOptions, { label = fav, icon = 'trash' })
+    end
+
+    lib.registerMenu({
+        id = 'remove_favourites_menu',
+        title = 'Remove from Favourites',
+        position = Config.MenuPosition,
+        options = removeOptions
+    }, function(selected)
+        local soundToRemove = removeOptions[selected].label
+        Favourites[soundToRemove] = nil
+        saveFavourites()
+        Config.Notify('Engine sound removed from favourites!', 'success')
+        if next(Favourites) == nil then
+            Config.Notify('You have no more favourites!', 'info')
+            showEngineSoundMenu()
+        else
+            showRemoveFavouritesMenu()
+        end
+    end)
+
+    lib.showMenu('remove_favourites_menu')
+end
+
+local function showFavouritesMenu()
+    if next(Favourites) == nil then
+        Config.Notify('You don\'t have any favourites!', 'error')
+        return
+    end
+
+    local favouriteOptions = {}
+    for fav, _ in pairs(Favourites) do
+        table.insert(favouriteOptions, { label = fav, icon = 'star' })
+    end
+    table.insert(favouriteOptions, { label = 'Remove from Favourites', icon = 'trash' })
+
+    lib.registerMenu({
+        id = 'favourites_menu',
+        title = 'Favourites',
+        position = Config.MenuPosition,
+        options = favouriteOptions
+    }, function(selected)
+        local selectedFav = favouriteOptions[selected].label
+        if selectedFav == 'Remove from Favourites' then
+            showRemoveFavouritesMenu()
+        else
+            if not cache.vehicle or cache.seat ~= -1 then
+                return Config.Notify('You need to be driving a vehicle to use this!', 'error')
+            end
+
+            TriggerServerEvent('Chroma:EngineSounds:ChangeEngineSound', {
+                net = VehToNet(cache.vehicle),
+                sound = Config.EngineSounds[selectedFav]
+            })
+
+            Config.Notify(string.format('Engine sound changed to: %s', selectedFav), 'success')
+            if not Config.CloseOnSelect then
+                lib.showMenu('favourites_menu')
+            end
+        end
+    end)
+
+    lib.showMenu('favourites_menu')
+end
 
 lib.registerMenu({
     id = 'engine_sound_menu',
     title = 'Engine Sound Menu',
     position = Config.MenuPosition,
-    onSideScroll = function(selected, scrollIndex, args)
+    onSideScroll = function(_, scrollIndex)
         Index = scrollIndex
     end,
     options = {
@@ -32,7 +112,7 @@ lib.registerMenu({
         { label = 'Add to Favourites', icon = 'heart' },
         { label = 'View Favourites', icon = 'star' }
     }
-}, function(selected, scrollIndex, args)
+}, function(selected, scrollIndex)
     if selected == 1 then
         if not cache.vehicle or cache.seat ~= -1 then
             return Config.Notify('You need to be driving a vehicle to use this!', 'error')
@@ -56,59 +136,11 @@ lib.registerMenu({
         else
             Config.Notify('This engine sound is already in your favourites!', 'error')
         end
-        if Config.CloseOnSelect then
-            lib.hideMenu('engine_sound_menu')
+        if not Config.CloseOnSelect then
+            showEngineSoundMenu()
         end
     elseif selected == 3 then
-        local favouriteOptions = {}
-        for fav, _ in pairs(Favourites) do
-            table.insert(favouriteOptions, { label = fav, icon = 'star' })
-        end
-        table.insert(favouriteOptions, { label = 'Remove from Favourites', icon = 'trash' })
-        lib.registerMenu({
-            id = 'favourites_menu',
-            title = 'Favourites',
-            position = Config.MenuPosition,
-            options = favouriteOptions
-        }, function(selected, scrollIndex, args)
-            local selectedFav = favouriteOptions[selected].label
-            if selectedFav == 'Remove from Favourites' then
-                local removeOptions = {}
-                for fav, _ in pairs(Favourites) do
-                    table.insert(removeOptions, { label = fav, icon = 'trash' })
-                end
-                lib.registerMenu({
-                    id = 'remove_favourites_menu',
-                    title = 'Remove from Favourites',
-                    position = Config.MenuPosition,
-                    options = removeOptions
-                }, function(selected, scrollIndex, args)
-                    local soundToRemove = removeOptions[selected].label
-                    Favourites[soundToRemove] = nil
-                    saveFavourites()
-                    Config.Notify('Engine sound removed from favourites!', 'success')
-                    if Config.CloseOnSelect then
-                        lib.hideMenu('remove_favourites_menu')
-                    end
-                end)
-                lib.showMenu('remove_favourites_menu')
-            else
-                if not cache.vehicle or cache.seat ~= -1 then
-                    return Config.Notify('You need to be driving a vehicle to use this!', 'error')
-                end
-
-                TriggerServerEvent('Chroma:EngineSounds:ChangeEngineSound', {
-                    net = VehToNet(cache.vehicle),
-                    sound = Config.EngineSounds[selectedFav]
-                })
-
-                Config.Notify(string.format('Engine sound changed to: %s', selectedFav), 'success')
-                if Config.CloseOnSelect then
-                    lib.hideMenu('favourites_menu')
-                end
-            end
-        end)
-        lib.showMenu('favourites_menu')
+        showFavouritesMenu()
     end
 end)
 
@@ -116,30 +148,20 @@ RegisterNetEvent("Chroma:EngineSounds:OpenMenu", function()
     if not cache.vehicle or cache.seat ~= -1 then
         return Config.Notify('You need to be driving a vehicle to use this!', 'error')
     end
-
-    lib.setMenuOptions('engine_sound_menu',
-        {
-            label = 'Change Engine Sound',
-            icon = 'arrows-up-down-left-right',
-            values = DisplayLabels,
-            defaultIndex = Index,
-            close = Config.CloseOnSelect
-        },
-        1)
-    lib.showMenu('engine_sound_menu')
+    showEngineSoundMenu()
 end)
 
-AddStateBagChangeHandler("vehdata:sound", nil, function(bagName, key, value)
+AddStateBagChangeHandler("vehdata:sound", nil, function(bagName, _, value)
     local entity = GetEntityFromStateBagName(bagName)
     if entity == 0 then return end
     ForceUseAudioGameObject(entity, value)
 end)
 
-local keybind = lib.addKeybind({
+lib.addKeybind({
     name = 'open_enginesound_menu',
     description = 'Open Engine Sound Menu',
     defaultKey = Config.Keybind,
-    onPressed = function(self)
+    onPressed = function()
         ExecuteCommand('enginesound')
     end,
 })
--- chroma-enginesoundmenu/server.lua
+++ chroma-enginesoundmenu/server.lua
@@ -1,36 +1,3 @@
-local FavouritesFile = 'favourites.json'
-local Favourites = {}
-
-local function loadFavourites()
-    local file = LoadResourceFile(GetCurrentResourceName(), FavouritesFile)
-    if file then
-        Favourites = json.decode(file)
-    else
-        Favourites = {}
-    end
-end
-
-local function saveFavourites()
-    SaveResourceFile(GetCurrentResourceName(), FavouritesFile, json.encode(Favourites, { indent = true }), -1)
-end
-
-local function getPlayerIdentifier(src)
-    local identifiers = GetPlayerIdentifiers(src)
-    for _, identifier in ipairs(identifiers) do
-        if identifier:match('discord:') then
-            return identifier
-        end
-    end
-    for _, identifier in ipairs(identifiers) do
-        if identifier:match('license:') then
-            return identifier
-        end
-    end
-    return tostring(src)
-end
-
-loadFavourites()
-
 RegisterCommand("enginesound", function(source, args, rawCommand)
     if Config.HasPermission(source) then
         TriggerClientEvent("Chroma:EngineSounds:OpenMenu", source)
@@ -40,6 +7,7 @@ RegisterCommand("enginesound", function(source, args, rawCommand)
 end, false)
 
 RegisterServerEvent("Chroma:EngineSounds:ChangeEngineSound", function(data)
+
     if not Config.HasPermission(source) then
         return Config.BanPlayer(source)
     end
@@ -47,27 +15,18 @@ RegisterServerEvent("Chroma:EngineSounds:ChangeEngineSound", function(data)
     local entity = NetworkGetEntityFromNetworkId(data.net)
     if not DoesEntityExist(entity) then return end
     Entity(entity).state['vehdata:sound'] = data.sound
-end)
-
-RegisterServerEvent('Chroma:EngineSounds:SaveFavourites', function(favourites)
-    local playerId = getPlayerIdentifier(source)
-    Favourites[playerId] = favourites
-    saveFavourites()
-end)
-
-RegisterServerEvent('Chroma:EngineSounds:LoadFavourites', function()
-    local playerId = getPlayerIdentifier(source)
-    local playerFavourites = Favourites[playerId] or {}
-    TriggerClientEvent('Chroma:EngineSounds:ReceiveFavourites', source, playerFavourites)
+    
 end)
 
 CreateThread(function()
+    
     if Config.CheckForUpdates then
+
         -- https://github.com/Blumlaut/FiveM-Resource-Version-Check-Thing/
         updatePath = "/Gravxd/fivem-enginesound-menu" -- your git user/repo path
         resourceName = "^6chroma-enginesoundmenu" -- the resource name
         
-        local function checkVersion(err, responseText, headers)
+        local function checkVersion(err,responseText, headers)
             curVersion = LoadResourceFile(GetCurrentResourceName(), "version.txt") -- make sure the "version" file actually exists in your resource root!
             if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
                 print("\n"..resourceName.." ^1is outdated, please update it from:\n^3https://github.com/Gravxd/fivem-enginesound-menu/releases/latest\n^1For support or issues, please visit ^3https://discord.gg/chromalabs^7")
@@ -77,7 +36,9 @@ CreateThread(function()
         end
         
         PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/main/chroma-enginesoundmenu/version.txt", checkVersion, "GET")
+
     else
         print("\n^6chroma-enginesoundmenu ^2has been loaded - enjoy! ^1[VERSION CHECK DISABLED]\n^2For support or issues, please visit ^3https://discord.gg/chromalabs^7")
     end
+
 end)
